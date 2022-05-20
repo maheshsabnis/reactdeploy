f478dcb43a830a96eae68c15e519750fc17d315c
maheshsabnis <sabnis_m@hotmail.com>
2022-05-20 15:43:34 +0530

    2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const clickme=()=>{
+    alert('Clicked');
+  }
   return (
     <div className="App">
        <h1>Hello React.js Deploy</h1>
+       <button onClick={clickme}>Click Me</button>
     </div>
   );
 }
